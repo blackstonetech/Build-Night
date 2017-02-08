82236f80531ad2bf6adc21b2951d8acc543107fc
Bill Brooks <brookswm@gmail.com>
2017-02-08 22:07:33 +0000

    Created additional variables

--- Franz_Koffeeka/src/subscribe/mqttSubBasic.py
+++ Franz_Koffeeka/src/subscribe/mqttSubBasic.py
@@ -11,7 +11,13 @@ def on_connect(client, userdata, flags, rc):
   
 def on_message(client, userdata, msg):
   epochtime = time.time()
-  iocinput = (str(epochtime) + "|" + str(msg.payload.decode("utf-8")))
+  rawmessage = str(msg.payload.decode("utf-8"))
+  quality = rawmessage[0]
+  brewing = rawmessage[2]
+  strength = rawmessage[4:5]
+  level = rawmessage[7]
+#  iocinput = (str(epochtime) + "|" + str(msg.payload.decode("utf-8")))
+  iocinput = str(epochtime) + "|" + quality + "|" + brewing + "|" + strength + "|" + level
   print(msg.topic + " | " + str(msg.payload.decode("utf-8")))
   filepath = os.path.join('/home/pi', 'coffee.log')
   with open(filepath, "a") as f:
@@ -19,7 +25,8 @@ def on_message(client, userdata, msg):
     f.close()
   
 client = mqtt.Client()
-client.connect("192.168.10.6", 1883,60)
+#client.connect("192.168.10.6", 1883,60)
+client.connect("192.168.10.2", 1883,60)
 
 client.on_connect = on_connect
 client.on_message = on_message
