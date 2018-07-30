9aad065e87a19fdcd7d5a7f0eb62332c992caa15
Elise Walker <elwalker@bstonetech.com>
2018-07-30 16:29:22 -0400

    Added deployment documentation

--- Franz_Koffeeka/README.md
+++ Franz_Koffeeka/README.md
@@ -1,28 +1,113 @@
 # Franz Koffeeka
+
 The Internet of Coffee Project - Monitor the Office Coffee Pot
 
-## Objective:
-Monitor the state of the Office Coffee Maker:
+## Objective
+
+Monitor the state of the Office Coffee Maker
+
 * report on the amount of coffee in the pot
 * report on when the pot was brewed
 * report on the strength of the coffee in the pot
 * other stretch goals like temperature of the coffee
 
-## Deploying Project:
+## Architecture
 
+The coffee IoT project is comprised of 4 logical elements.
 
-### Arduino Reading:
-- Intro https://learn.adafruit.com/ladyadas-learn-arduino-lesson-number-1
-- Multi-tasking https://learn.adafruit.com/multi-tasking-the-arduino-part-1
-- MQTT (pub/sub):  https://learn.adafruit.com/diy-esp8266-home-security-with-lua-and-mqtt
+* Coffee Sensor Array (publish)
+* MQTT Message Broker (Open-Source Mosquitto)
+* Python Subscription Service (subscribe)
+* Python Presentation Service (present)
 
+The sensor array is powered by an Adafruit feather, which is programmed in Arduino C. The sensor array collects information about the coffee pot based on sensors attached around the pot.
 
-### Raspberry Pi Reading:
-- Intro https://www.raspberrypi.org/forums/viewtopic.php?t=4751
-- SSH https://tutorials-raspberrypi.com/raspberry-pi-remote-access-by-using-ssh-and-putty/
-- MQTT (pub/sub) https://learn.adafruit.com/diy-esp8266-home-security-with-lua-and-mqtt/configuring-mqtt-on-the-raspberry-pi
+The Message Broker, Mosquitto, acts as a queue for capturing and publishing messages sent by the publish software. If you aren't familiar with how a message queue works; its a stack of information that is stored until it can be processed later. The information is pulled off the stack when its processed.
+
+The subscription service listens to the queue and stores items in the queue to a file locally. Those files are formatted for use by the presentation service.
+
+The presentation service contains the business logic for the application. It parses through the file created by the subscription service and presents to the coffee display based on logic contained within itself.
+
+## Deployment
+
+### Coffee Sensor Publish Software
+
+The code can be found in the publish directory. Deploy the code like any other arduino code, by uploading the code through the Arduino IDE.
+
+### MQTT Message Broker
+
+Mosquitto should be deployed on the Raspberry PI 3. The software can be found: https://mosquitto.org
+
+The website explains the installation.
+
+Once installed, mosquitto needs to be setup to run. The broker can be manually run by executing
+
+    mosquitto -d
+
+Mosquitto can be set to run by default by adding the mosquitto -d command to
+
+    /etc/rc.local
+
+### Subscription Service
+
+The subscription service is a python application that can be found in the subscribe folder. Run:
+
+    python mqttSubBasic.py
+
+or create a service definition to deploy at startup.
+
+Sample service definition file (/etc/systemd/system/iotpresent.service)
+
+        [Unit]
+        Description=Coffee IoT Subscribe
+        After=multi-user.target
+
+        [Service]
+        Type=idle
+        ExecStart=/usr/bin/python3 /home/pi/Federal-IoT/Franz_Koffeeka/src/subscribe/mqttSubBasic.py
+
+        [Install]
+        WantedBy=multi-user.target
+
+## Present Service
+
+Also a python application, the service can be found in the present folder. Run:
+
+    python coffee.py
+
+or create a service definition to deploy.
+
+Sample service definition file (/etc/systemd/system/iotpresent.service)
+
+        [Unit]
+        Description=Coffee IoT Present
+        Wants=iotsubscribe.service
+        After=multi-user.target iotsubscribe.service
+
+        [Service]
+        Type=idle
+        ExecStart=/usr/bin/python /home/pi/Federal-IoT/Franz_Koffeeka/src/present/coffee.py
+        Restart=always
+
+        [Install]
+        WantedBy=multi-user.target
+
+## More Information
+
+### Arduino Reading
+
+* Intro https://learn.adafruit.com/ladyadas-learn-arduino-lesson-number-1
+* Multi-tasking https://learn.adafruit.com/multi-tasking-the-arduino-part-1
+* MQTT (pub/sub):  https://learn.adafruit.com/diy-esp8266-home-security-with-lua-and-mqtt
+
+### Raspberry Pi Reading
+
+* Intro https://www.raspberrypi.org/forums/viewtopic.php?t=4751
+* SSH https://tutorials-raspberrypi.com/raspberry-pi-remote-access-by-using-ssh-and-putty/
+* MQTT (pub/sub) https://learn.adafruit.com/diy-esp8266-home-security-with-lua-and-mqtt/configuring-mqtt-on-the-raspberry-pi
 
 ## All Materials Experimented With
+
 Tutorials: [[http://www.adafruit.com/products/2821#tutorials]]
 
 x Assembled Adafruit Feather HUZZAH ESP8266 WiFi
@@ -47,20 +132,20 @@ Component bag containing:
 1x Photo Cell Light Sensor
 1x Diffused RGB (tri-color) LED
 1x Breadboard-friendly SPDT Slide Switch
-```
- Tutorials: [[http://www.adafruit.com/products/3032#tutorials]]
+
+Tutorials: [[http://www.adafruit.com/products/3032#tutorials]]
 Melexis Contact-less Infrared Sensor - MLX90614 5V
- Tutorials: [[http://www.adafruit.com/products/1748#tutorials]]
+Tutorials: [[http://www.adafruit.com/products/1748#tutorials]]
 IR Break Beam Sensor - 3mm LEDs
- Tutorials: [[http://www.adafruit.com/products/2167#tutorials]]
+Tutorials: [[http://www.adafruit.com/products/2167#tutorials]]
 Photo Transistor Light Sensor
- Tutorials: [[http://www.adafruit.com/products/2831#tutorials]]
+Tutorials: [[http://www.adafruit.com/products/2831#tutorials]]
 Contact-less Infrared Thermopile Sensor Breakout - TMP007
- Tutorials: [[http://www.adafruit.com/products/2023#tutorials]]
+Tutorials: [[http://www.adafruit.com/products/2023#tutorials]]
 IR Break Beam Sensor - 5mm LEDs
- Tutorials: [[http://www.adafruit.com/products/2168#tutorials]]
+Tutorials: [[http://www.adafruit.com/products/2168#tutorials]]
 RGB Color Sensor with IR filter and White LED - TCS34725
- Tutorials: [[http://www.adafruit.com/products/1334#tutorials]]
+Tutorials: [[http://www.adafruit.com/products/1334#tutorials]]
 
 * Rasberry Pi 3 model B
 * 32x32 "pixel" display panels
--- Franz_Koffeeka/src/present/coffee.py
+++ Franz_Koffeeka/src/present/coffee.py
@@ -34,7 +34,6 @@ matrix = Adafruit_RGBmatrix(32, 2)
 
 #TODO add a switch where it will replay the file and then start tailing, otherwise, just pass until last line and wait on tail
 
-
 ##  Right panel shows coffee pot level:
 ##     empty (3) - empty, red (2) - low, yellow (1) - half, green (0) - full
 ##   and strength: 0 - no drip, 1 - left drip, 2 - right drip, 3 - both drips
@@ -49,12 +48,7 @@ matrix = Adafruit_RGBmatrix(32, 2)
 ##     flashes white brewing (0), black (1), pink brewing (2) to make a flashing sign affect
 
 def presentStatus(row):
-    # left_image.insert(0,'Black.png')
-    # left_image.insert(1,'Black.png')
-    # left_image.insert(2,'Black.png')
-    # right_image.insert(0,'Black.png')
-    # right_image.insert(1,'None')
-    # right_image.insert(2,'None')
+
 # initialize to black, but then right 2 & 3 need None for logic below
     for img in range(3):
        left_image[img] = IMAGE_BLACK
@@ -192,164 +186,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-##### Holding on to some other attempts at code before clean-up. Delete for production ####
-
-##def main():
-##    print("here")
-##    p = OptionParser("usage: tail.py file")
-##    (options, args) = p.parse_args()
-###    if len(args) < 1:
-###        p.error("must specify a file to watch")
-##
-##    csvFile = os.path.join(CSV_FILE_PATH, CSV_FILE_NAME)
-##
-##    fieldnames = ['epoch_time', 'last_brew_time', 'quality', 'brewing', 'strength', 'level']
-##
-####    with open(csvFile, 'rb') as csvfile:
-####         reader = csv.DictReader(csvfile,delimiter='|', fieldnames=fieldnames, restval='n')
-####         for row in reader:
-####             print(row)
-#####         epochtime = time.strftime('%Y-%m-%d %H:%M:%S', float(row['epoch_time']))
-####             print(row['epoch_time'], row['last_brew_time'], row['quality'],row['brewing'],row['strength'],row['level'])
-#####         print(epoch_time, row['last_brew_time'], row['quality'],row['brewing'],row['strength'],row['level'])
-##### note may work to get it off a string var: for row in csv.reader(['one,two,three']):
-###    with open(args[0], 'r') as fin:
-##    print("here")
-##    with open(csvFile, 'r') as fin:
-##        reader = csv.DictReader(fin, delimiter='|', fieldnames=fieldnames, restval='n')
-###        for line in readlines_then_tail(fin):
-###        for line in readlines_then_tail(reader):
-### "New code scheme - read through file here"
-##        for line in reader:
-##            print("read line==>" + str(line) + "<==")
-####            print(line.strip())
-### this doesn't work?            if line:
-##            if 1 == 1:
-##                print("skip line==>" + str(line) + "<==")
-##                presentStatus(line)
-###                yield line
-##            else:
-### do this once working                tail(fin)
-##                presentStatus(line)
-###Logic to handle the epoch time
-##    epochtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
-##    print(epochtime)
-##
-
-## Keeping as it is another perspective with animation     
-##if  row['brewing'] == 't':
-##    left_image[0] = 'Brewing_Pink.png'
-##    left_image[1] = 'Black.png'
-##    left_image[2] = 'Brewing.png'
-##if row['level'] == '0':
-##    if row['strength'] == '0':
-##    right_image[0] = 'Empty_0.png'
-##    right_image[1] = 'Empty_1.png'
-##    right_image[2] = 'Empty_2.png'
-##elif row['level'] == '1':
-##    right_image[0] = 'Red_0.png'
-##    right_image[1] = 'Red_1.png'
-##    right_image[2] = 'Red_2.png'
-##elif row['level'] == '2':
-##    right_image[0] = 'Yellow_0.png'
-##    right_image[1] = 'Yellow_1.png'
-##    right_image[2] = 'Yellow_2.png'
-##elif row['level'] == '3':
-##    right_image[0] = 'Green_0.png'
-##    right_image[1] = 'Green_1.png'
-##    right_image[2] = 'Green_2.png'
-##else :
-##    left_image[0] = 'Brewing_Pink.png'
-##    left_image[1] = 'Black.png'
-##    left_image[2] = 'Brewing.png'
-##        
-
-
-##     image = Image.open("Empty_1.png")
-## image.load()          # Must do this before SetImage() calls
-#matrix.Fill(0x6F85FF) # Fill screen to sky color
-#for n in range(32, -image.size[0], -1): # Scroll R to L
-## matrix.SetImage(image.im.id, 0, 0)
-## time.sleep(2.0)
-
-#Opening and reading from the file in python
-#with open('testdata.txt', 'rb') as csvfile:
-## with open(myfile, 'rb') as csvfile:
-##   spamreader = csv.reader(csvfile, delimiter='|')
-##   for row in spamreader:
-##      print row
-##      epoch = row[1]   
-##
-##print epoch
-
-# Flash screen red, green, blue (packed color values)
-
-
-# Show RGB test pattern (separate R, G, B color values)
-#for b in range(16):
-#        for g in range(8):
-#                for r in range(8):
-#                        matrix.SetPixel(
-#                          (b / 4) * 8 + g,
-#                          (b & 3) * 8 + r,
-#                          (r * 0b001001001) / 2,
-#                          (g * 0b001001001) / 2,
-#                           b * 0b00010001)
-
-#matrix.SetPixel(32, 0, 90, 110, 15);
-
-#time.sleep(10.0)
-#matrix.Clear()
-
-##    with open(csvFile, 'rb') as csvfile:
-##         reader = csv.DictReader(csvfile,delimiter='|', fieldnames=fieldnames, restval='n')
-##         for row in reader:
-##             print(row)
-###         epochtime = time.strftime('%Y-%m-%d %H:%M:%S', float(row['epoch_time']))
-##             print(row['epoch_time'], row['last_brew_time'], row['quality'],row['brewing'],row['strength'],row['level'])
-###         print(epoch_time, row['last_brew_time'], row['quality'],row['brewing'],row['strength'],row['level'])
-### note may work to get it off a string var: for row in csv.reader(['one,two,three']):
-#    with open(args[0], 'r') as fin:
-##    print("here")
-##    epoch_timeIndex = fieldnames.index("epoch_time")
-##    last_brew_timeIndex = fieldnames.index("last_brew_time")
-##    qualityIndex = fieldnames.index("quality")
-##    brewingIndex = fieldnames.index("brewing")
-##    strengthIndex = fieldnames.index("strength")
-##    levelIndex = fieldnames.index("level")
-
-##            try:
-##                line = next(reader)
-##            except StopIteration:
-##                print("stopiteration")
-
-### def readlines_then_tail(fin):
-##def readlines_then_tail(line):
-##    "Iterate through lines and then tail for further lines."
-##    while True:
-###        line = fin.readline()
-###        line = next(fin)
-##        print("next line==>" + str(line) + "<==")
-##        if line:
-##            yield line
-##        else:
-##            
-##            tail(fin)
-##
-##def tail(fin):
-##    "Listen for new lines added to file."
-##    print("in tail")
-##    while True:
-##        where = fin.tell()
-###        line = fin.readline()
-##        print("in loop, where :" + str(where))
-##        line = next(fin)
-##        print("tail next line==>" + str(line) + "<==")
-##        if not line:
-##            time.sleep(SLEEP_INTERVAL)
-##            fin.seek(where)
-##        else:
-##            yield line
-##
-
